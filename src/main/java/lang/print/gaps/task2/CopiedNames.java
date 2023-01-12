da6dc6fdafde12841006058bbc41b315d805ee62
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-01-12 13:49:50 +0600

    done 3

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,20 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        System.out.println(variable);
         int variable2 = 2000;
-        System.out.println(variable2);
         int value = 1000;
-        System.out.println(value);
         int variable3 = 4000;
-        System.out.println(variable3);
         int value2 = 3000;
-        System.out.println(value2);
-        int variables1 = 5000;
-        System.out.println(variables1);
-        variables1 = 7000;
-        System.out.println(variables1);
+        int variable4 = 5000;
+        int variable5 = 7000;
         int value3 = 90000;
-        System.out.println(value3);
     }
 }
--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -6,10 +6,9 @@ public class JavaVariableNames {
         int money = 10;
         int hundred = 100;
         int back = 1000;
-        int under_Score = 10000;
+        int underScore = 10000;
         int sharp = 100000;
         int star = 1000000;
         int goodVarName = 1000000;
-        System.out.println(tomorrow+""+money+""+hundred+""+back+""+under_Score+""+sharp+""+star+""+goodVarName);
     }
 }
